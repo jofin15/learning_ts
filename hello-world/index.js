cee5e14d8c6c62c751b2868c87a1fb2a501d0068
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-07-24 13:04:55 +0000

    first commit

---
+++ hello-world/index.js
@@ -0,0 +1,2 @@
+console.log("hello world");
+var age = 18; //by default typescript compiler uses old version of javascript called ecmaScript5. That is why var
---
+++ hello-world/index.ts
@@ -0,0 +1,4 @@
+console.log("hello world");
+
+let age: number = 18;
+
