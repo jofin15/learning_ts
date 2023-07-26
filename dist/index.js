eade2a9722bdd86e2c0864c783add941b2e3df17
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-07-26 10:54:35 +0000

    tuple commit

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,17 @@
 let sales = 123456;
 let course = "Angular";
 let is_pulished = true;
+let level;
+level = 10;
+level = "a";
+function render(document) {
+    console.log(document);
+}
+let arr = [1, 2, 3, 4, 5, 6];
+let user = [1, "jofin"];
+let user2 = [2, "jomin"];
+let user3 = [3, "johnson"];
+let user4 = [4, "james"];
+user.push(7, "james");
+console.log(user);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -6,3 +6,29 @@ let level;
 level = 10;
 level = "a";
 
+
+//note :- avoid using "any" type as much as possible
+
+function render(document:any){
+console.log(document);
+
+}
+
+
+//Arrays
+let arr:number[] = [1,2,3,4,5,6];
+
+
+
+
+//tuples:- its a fixed length array where each element is of a specific type .Also use tuples when have only 2 elements (key and value).
+let user:[number,string]=[1,"jofin"]
+let user2:[number,string]=[2,"jomin"]
+let user3:[number,string]=[3,"johnson"]
+let user4:[number,string]=[4,"james"]
+
+user.push(7,"james");
+console.log(user);
+
+// let lastIndexOf =user[1].lastIndexOf("n");
+// console.log(lastIndexOf);
